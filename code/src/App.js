61151b23ddbfac6ae44ed1d880756e5ea40a053a
wupeng <979235214@qq.com>
2018-04-02 21:22:40 +0800

    1

--- code/src/App.js
+++ code/src/App.js
@@ -1,21 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './app/Home'
+import About from './app/About'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello Word</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+       <div>
+        <ul>
+         <li><Link to="/">首页</Link></li>
+         <li><Link to="/about">关于</Link></li>
+         <li><Link to="/topices">主题列表</Link></li>
+        </ul>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/topices" component={Topics} />
+       </div>
+      </Router>
     );
   }
 }
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>主题列表</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          使用react熏染
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          组件
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          属性 v. 状态
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/wUPENG`}>
+          属性 v. 状态wUPENG
+        </Link>
+      </li>
+    </ul>
+    <hr />
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>选择主题</h3>
+    )}/>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 export default App;
---
+++ code/src/app/About/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return(
+            <div><h1 style={{ color: 'red' }}>关于</h1></div>
+        )
+    }
+}
+export default About;
