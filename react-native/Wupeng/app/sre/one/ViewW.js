74b03fb27b80b13c8c4e21dc7527523d74d894f7
wupeng <979235214@qq.com>
2018-05-12 17:15:22 +0800

    node测试

---
+++ react-native/Wupeng/app/sre/one/ViewW.js
@@ -0,0 +1,28 @@
+/*
+ * @Author: Wupeng 
+ * @Date: 2018-05-09 14:23:57 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-05-09 14:27:39
+ * View 组件
+ */
+import React, { Component } from 'react'
+import {
+    Text,
+    View,
+    StyleSheet,
+} from 'react-native'
+
+export default class ViewW extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return (
+            <View><Text>1111111</Text></View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    
+})
