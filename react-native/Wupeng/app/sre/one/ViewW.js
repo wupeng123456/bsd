bc4519cb524279079ad36ee460405cb9dd7547d1
Wupeng <979235214@qq.com>
2018-05-13 23:20:02 +0800

    react-native_View

--- react-native/Wupeng/app/sre/one/ViewW.js
+++ react-native/Wupeng/app/sre/one/ViewW.js
@@ -19,10 +19,73 @@ export default class ViewW extends Component {
     }
     render() {
         return (
-            <View><Text>1111111</Text></View>
+            <View style={styles.container}>
+                <View style={[styles.item, styles.center, styles.border1]}>
+                    <Text>酒店</Text>
+                </View>
+                <View style={styles.item}>
+                    <View style={[styles.flex, styles.center, styles.border5]}>
+                        <Text>海外酒店</Text>
+                    </View>
+                    <View style={[styles.flex, styles.center]}>
+                        <Text>特惠酒店</Text>
+                    </View>
+                </View>
+                <View style={[styles.item, styles.border4]}>
+                    <View style={[styles.flex, styles.center, styles.border2]}>
+                        <Text>客栈</Text>
+                    </View>
+                    <View style={[styles.flex, styles.center, styles.border3]}>
+                        <Text>特惠</Text>
+                    </View>
+                </View>
+            </View>
         )
     }
 }
 const styles = StyleSheet.create({
-    
+    container:{
+        flex: 1,
+        borderWidth: 1,
+        // borderColor: 'red',
+        flexDirection: 'row',
+        backgroundColor: 'white',
+    },
+    item: {
+        flex: 1,
+        height: 80,
+        // borderWidth: 1,
+        // borderColor: 'blue',
+    },
+    center: {
+        justifyContent: 'center', //justifyContent
+        alignItems: 'center',
+        backgroundColor: '#f631bb',
+    },
+    flex: {
+        flex: 1,
+        // backgroundColor: '#f631bb',
+    },
+    border1: {
+        borderTopLeftRadius: 5,
+        borderBottomLeftRadius:5,
+        borderRightWidth: 1,
+        borderRightColor: 'white',
+    },
+    border2: {
+        borderTopRightRadius: 5,
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'white',
+    },
+    border3: {
+        borderBottomRightRadius: 5,
+    },
+    border4: {
+        borderLeftWidth: 1,
+        borderLeftColor: 'white',
+    },
+    border5: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'white',
+    },
 })
