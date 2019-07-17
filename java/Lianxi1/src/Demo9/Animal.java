5e6f5f1e88de5a1aef2c0732e6e156c6aba56072
wupeng <979235214@qq.com>
2019-07-17 17:14:46 +0800

    提交代码县里

---
+++ java/Lianxi1/src/Demo9/Animal.java
@@ -0,0 +1,5 @@
+package Demo9;
+
+public class Animal {
+
+}
---
+++ java/Lianxi1/src/Demo9/Cat.java
@@ -0,0 +1,10 @@
+package Demo9;
+
+public class Cat extends Animal {
+	void catchMouse() {
+		System.out.println("×¥ÀÏÊó");
+	}
+	void eat() {
+		System.out.println("³ÔĞ¡Óã");
+	}
+}
